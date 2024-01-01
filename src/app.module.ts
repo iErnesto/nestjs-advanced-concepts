0c6fa6977b7784bd764f4fc5452338055df48c1f
iErnesto <ernesto1991diaz@gmail.com>
2024-01-01 15:55:21 -0600

    Add worker threads

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { SchedulerModule } from './scheduler/scheduler.module';
+import { FibonacciModule } from './fibonacci/fibonacci.module';
 
 @Module({
-  imports: [CoffeesModule, SchedulerModule],
+  imports: [CoffeesModule, SchedulerModule, FibonacciModule],
   controllers: [AppController],
   providers: [AppService],
 })
